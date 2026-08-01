6a95c52e6fadcfa708edbc12c6d95393cd11ef7b
Kuolmarko24 <kuolmarko47@gmail.com>
2026-08-01 14:58:00 +0200

    Views Created

--- Tables.sql
+++ Tables.sql
@@ -915,3 +915,133 @@ GROUP BY
 d.first_name,
 d.last_name,
 d.specialization;
+
+-- views --
+-- view for the patient
+CREATE VIEW vw_patient_demographics AS 
+SELECT
+	patient_id,
+	first_name,
+	last_name,
+	gender,
+	date_of_birth,
+	insurance_provider
+FROM patients_cleaned;
+
+
+-- view for the doctor
+CREATE VIEW vw_doctor_information AS
+SELECT 
+doctor_id,
+first_name,
+last_name,
+specialization,
+years_experience,
+hospital_branch,
+phone_number,
+email
+FROM doctors_cleaned;
+
+-- view for appointment
+CREATE VIEW vw_appointment_details AS
+SELECT
+    a.appointment_id,
+    a.patient_id,
+    p.first_name + ' ' + p.last_name AS PatientName,
+    a.doctor_id,
+    d.first_name + ' ' + d.last_name AS DoctorName,
+    d.specialization,
+    d.hospital_branch,
+    a.appointment_date,
+    a.appointment_time,
+    a.reason_for_visit,
+    a.status
+FROM appointments_cleaned a
+INNER JOIN patients_cleaned p
+    ON a.patient_id = p.patient_id
+INNER JOIN doctors_cleaned d
+    ON a.doctor_id = d.doctor_id;
+
+-- treatment analytics view --
+CREATE VIEW vw_treatment_analytics AS
+SELECT
+    t.treatment_id,
+    t.appointment_id,
+    a.patient_id,
+    p.first_name + ' ' + p.last_name AS PatientName,
+    a.doctor_id,
+    d.first_name + ' ' + d.last_name AS DoctorName,
+    d.specialization,
+    d.hospital_branch,
+    t.treatment_type,
+    t.description,
+    t.cost,
+    t.treatment_date
+FROM treatment_cleaned t
+INNER JOIN appointments_cleaned a
+    ON t.appointment_id = a.appointment_id
+INNER JOIN patients_cleaned p
+    ON a.patient_id = p.patient_id
+INNER JOIN doctors_cleaned d
+    ON a.doctor_id = d.doctor_id;
+
+-- billing and revenue view --
+CREATE VIEW vw_billing_revenue AS
+SELECT
+    b.bill_id,
+    b.patient_id,
+    p.first_name + ' ' + p.last_name AS PatientName,
+    p.insurance_provider,
+    b.treatment_id,
+    t.treatment_type,
+    b.bill_date,
+    b.amount,
+    b.payment_method,
+    b.payment_status
+FROM billing_cleaned b
+INNER JOIN patients_cleaned p
+    ON b.patient_id = p.patient_id
+INNER JOIN treatment_cleaned t
+    ON b.treatment_id = t.treatment_id;
+
+-- executive dashboard View --
+CREATE VIEW vw_executive_dashboard AS
+SELECT
+    p.patient_id,
+    p.first_name + ' ' + p.last_name AS PatientName,
+    p.gender,
+    p.insurance_provider,
+
+    d.doctor_id,
+    d.first_name + ' ' + d.last_name AS DoctorName,
+    d.specialization,
+    d.hospital_branch,
+    d.years_experience,
+
+    a.appointment_id,
+    a.appointment_date,
+    a.appointment_time,
+    a.reason_for_visit,
+    a.status,
+
+    t.treatment_id,
+    t.treatment_type,
+    t.description,
+    t.cost,
+
+    b.bill_id,
+    b.amount,
+    b.payment_method,
+    b.payment_status
+
+FROM patients_cleaned p
+INNER JOIN appointments_cleaned a
+    ON p.patient_id = a.patient_id
+INNER JOIN doctors_cleaned d
+    ON a.doctor_id = d.doctor_id
+INNER JOIN treatment_cleaned t
+    ON a.appointment_id = t.appointment_id
+INNER JOIN billing_cleaned b
+    ON t.treatment_id = b.treatment_id;
+
+-- end of views --
